6640ac9da9e878cf9fde14dadf33ddaf952edec0
ThibautG <thibaut.gaudinat@gmail.com>
2025-01-28 12:38:43 +0100

    déplacer bouton reset HTML et affichage dynamique du prix total

--- index.js
+++ index.js
@@ -14,15 +14,29 @@ const total = document.getElementById("total");
 const reset =document.getElementById("reset");
 
 // affichage dynamique du tableau
-// transformation de l'objet js en html
+    // transformation de l'objet js en html
 const produitHTML = produits.map(produit =>
     `<tr>
-            <td>${produit.id}</td>
-            <td>${produit.nom}</td>
-            <td>${produit.prix}</td>
-            <td><button type="reset">Supprimer</button></td>
-        </tr>`
+        <td>${produit.id}</td>
+        <td>${produit.nom}</td>
+        <td class="prix">${produit.prix}</td>
+        <td><button type="reset" class="btn">Supprimer</button></td>
+      </tr>`
 );
 console.log(produitHTML);
-// insertion dans l'index.html
+    // insertion dans index.html
 tableau.innerHTML = produitHTML;
+
+
+// affichage du total des prix
+    // récupération des prix affichés en HTML depuis JS
+let prix = document.querySelectorAll(".prix");
+    // on crée un tableau vide dans lequel on vient push le contenu HTML présent entre les balises de class "prix"
+let prixTab = [];
+prix.forEach((x) => {
+   prixTab.push(Number(x.textContent));
+})
+    // calcul du prix total des produits affichés en utilisant reduce
+let totalPrix = prixTab.reduce((acc, v) => acc + v);
+    // insertion HTML du résultat dans la div class="total"
+total.innerHTML = `Votre panier affiche un total de ${totalPrix}€`
