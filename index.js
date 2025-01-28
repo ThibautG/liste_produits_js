d1f1a43192b3cd7b8a4d02af06f76b4d0ecef4c1
ThibautG <thibaut.gaudinat@gmail.com>
2025-01-28 11:14:58 +0100

    insertion de l'objet produits en HTML dans un tableau

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 // création de l'objet contenant les produits
 const produits = [
-    { id: 1, nom: "Café Espresso", prix: 2.5 },
-    { id: 2, nom: "Thé Vert Matcha", prix: 3.0 },
-    { id: 3, nom: "Chocolat Chaud", prix: 3.5 },
-    { id: 4, nom: "Café Latte", prix: 3.8 },
-    { id: 5, nom: "Thé Noir Earl Grey", prix: 2.7 },
-    { id: 6, nom: "Moka Caramel", prix: 4.2 }
+    { id: 1, nom: "Thé vert", prix: 12.99 },
+    { id: 2, nom: "Café Arabica", prix: 8.99 },
+    { id: 3, nom: "Infusion Camomille", prix: 5.49 },
+    { id: 4, nom: "Café Robusta", prix: 9.99 },
+    { id: 5, nom: "Thé Noir", prix: 7.99 },
 ];
 
+// création des 3 constantes pour l'affichage dynamique
+const recherche = document.getElementById("recherche");
+const tableau = document.getElementById("tableau");
+const total = document.getElementById("total");
+const reset =document.getElementById("reset");
+
+// affichage dynamique du tableau
+// transformation de l'objet js en html
+const produitHTML = produits.map(produit =>
+    `<tr>
+            <td>${produit.id}</td>
+            <td>${produit.nom}</td>
+            <td>${produit.prix}</td>
+            <td><button type="reset">Supprimer</button></td>
+        </tr>`
+);
+console.log(produitHTML);
+// insertion dans l'index.html
+tableau.innerHTML = produitHTML;
