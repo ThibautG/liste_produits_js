6fa62f6d8910ace6c2c4bac081630677b8572fd7
ThibautG <thibaut.gaudinat@gmail.com>
2025-01-28 09:57:39 +0100

    création tableau et champ de recherche en html + création de l'objet produit en js

---
+++ index.js
@@ -0,0 +1,10 @@
+// création de l'objet contenant les produits
+const produits = [
+    { id: 1, nom: "Café Espresso", prix: 2.5 },
+    { id: 2, nom: "Thé Vert Matcha", prix: 3.0 },
+    { id: 3, nom: "Chocolat Chaud", prix: 3.5 },
+    { id: 4, nom: "Café Latte", prix: 3.8 },
+    { id: 5, nom: "Thé Noir Earl Grey", prix: 2.7 },
+    { id: 6, nom: "Moka Caramel", prix: 4.2 }
+];
+
